16d0eb9b9e7d421dcbb36520c98201e56057f74e
Ilya <90723091+ilyaa07@users.noreply.github.com>
2024-04-02 16:27:31 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := device/samsung/gprimeltexx
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/nad_gprimeltexx.mk
+	$(LOCAL_DIR)/arrow_gprimeltexx.mk
 
 COMMON_LUNCH_CHOICES := \
-	nad_gprimeltexx-eng \
-	nad_gprimeltexx-userdebug \
-	nad_gprimeltexx-user
+	arrow_gprimeltexx-eng \
+	arrow_gprimeltexx-userdebug \
+	arrow_gprimeltexx-user
